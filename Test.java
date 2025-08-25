7c31182cc82075555079d4dcbeb5105f8f4b26cb
Phong-0508 <phongle050806@gmail.com>
2025-08-25 23:18:15 +0700

    init project

---
+++ Test.java
@@ -0,0 +1,16 @@
+/**
+ * @description Test.java
+ * @author Admin
+ * @version 1.0
+ * @created Aug 25, 2025
+ */
+package iuh.fit.oop;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
